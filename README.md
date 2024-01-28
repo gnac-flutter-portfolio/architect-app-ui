c793bb9aac910da68488ecfda4c2d87a3944a935
Serge GNACADJA <sergegnac2003@gmail.com>
2024-01-28 19:39:46 +0100

    Architect App UI/UX

---
+++ README.md
@@ -0,0 +1,6 @@
+# Architect App UI/UX
+
+### Drop a start ⭐️ if you find it useful.
+
+<img src="1.png" width="40%" height="40%"> <img src="2.png" width="40%" height="40%"> <img src="3.png" width="40%" height="40%">
+
---
+++ lib/home.dart
@@ -0,0 +1,260 @@
+import 'package:architect_ui/model.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  double pi = 22 / 1260;
+  int selectedIndex = 0;
+
+  Widget getListItems(ArchitectModel model) {
+    return Container(
+      width: 228,
+      margin: const EdgeInsets.only(right: 30),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  foregroundDecoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/${model.imageUrl}"),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                Positioned.directional(
+                  textDirection: TextDirection.ltr,
+                  end: 0,
+                  bottom: 1,
+                  child: ClipPath(
+                    clipper: _Hexagon(),
+                    child: Container(
+                      width: 70,
+                      height: 80,
+                      color: const Color(0xFFCD9274),
+                      child: Transform.rotate(
+                        angle: 180 * pi,
+                        child: const Center(
+                          child: Icon(
+                            Icons.keyboard_backspace,
+                            size: 37,
+                            color: Color(0xFFFADDC8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Text(
+                  model.name,
+                  maxLines: 1,
+                  overflow: TextOverflow.fade,
+                  style: const TextStyle(
+                    fontSize: 32,
+                    color: Color(0xFFfcf0e4),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Center(
+              child: Text(
+                model.description,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 21,
+                  color: Color(0xFFaacedc),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  model.price,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFecdfcf),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 18)
+        ],
+      ),
+    );
+  }
+
+  List<IconData> iconList = [
+    Icons.pie_chart_outline,
+    Icons.branding_watermark,
+    Icons.shopping_cart
+  ];
+
+  Widget getBottomBar(int index) {
+    return GestureDetector(
+      onTap: () {
+        selectedIndex = index;
+        setState(() {});
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            iconList[index],
+            color: index == selectedIndex
+                ? const Color(0xFFf6b889)
+                : const Color(0xFF568398),
+            size: 40,
+          ),
+          const SizedBox(height: 5),
+          Container(
+            width: 10,
+            height: 10,
+            decoration: BoxDecoration(
+                color: index == selectedIndex
+                    ? const Color(0xFFF6b889)
+                    : const Color(0xFF568398),
+                borderRadius: BorderRadius.circular(20)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color(0xFF406376),
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 36),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.settings,
+                            color: Color(0xFFF5B788), size: 32),
+                        Icon(Icons.blur_on, color: Color(0xFFF5B788), size: 32),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 30),
+                      Text(
+                        "Choose you own ready design",
+                        maxLines: 2,
+                        style:
+                            TextStyle(color: Color(0xFFFDF4e8), fontSize: 44),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: listModel
+                          .toList()
+                          .asMap()
+                          .entries
+                          .map((MapEntry map) {
+                        return getListItems(map.value);
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: const Color(0xFF223f50),
+                  padding: const EdgeInsets.symmetric(horizontal: 55),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: iconList.asMap().entries.map((MapEntry map) {
+                        return getBottomBar(map.key);
+                      }).toList()),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Hexagon extends CustomClipper<Path> {
+  double pi = 22 / 1260;
+  @override
+  Path getClip(Size size) {
+    double perpendicular = (size.width / 2) * tan(30 * pi);
+    double travel = (size.width / 2) / cos(30 * pi);
+
+    final path = Path();
+
+    path.lineTo(size.width / 2, 0);
+    path.lineTo(0, perpendicular);
+    path.lineTo(0, perpendicular + travel);
+    path.lineTo(size.width / 2, 2 * perpendicular + travel);
+    path.lineTo(size.width, perpendicular + travel);
+    path.lineTo(size.width, perpendicular);
+    path.lineTo(size.width / 2, 0);
+
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
